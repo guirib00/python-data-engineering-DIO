9b5517458d1ec110fb632dfd8845b3735812d4c4
Guilherme Ribeiro Souza <115327378+guirib00@users.noreply.github.com>
2024-10-04 21:50:30 -0300

    Herança simples e multipla

---
+++ modules/Mod3/HERANCA/poo_hands_heranca_simples.py
@@ -0,0 +1,37 @@
+class Veiculo:
+    def __init__(self, cor, placa, numero_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.numero_rodas = numero_rodas
+    
+    def ligar_motor(self):
+        print("motor ligado")
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}: {', '.join([f'{chave} = {valor}' for chave, valor in self.__dict__.items()])}"
+
+
+class Motocicleta(Veiculo):
+    pass
+
+class Carro(Veiculo):
+    pass
+
+class Caminhao(Veiculo):
+    def __init__(self, cor, placa, numero_rodas, carregado):
+        super().__init__(cor, placa, numero_rodas)
+        self.carregado = carregado
+        
+    def esta_carregado(self):
+        print(f"{'Sim' if self.carregado else 'Não'} estou carregado")
+
+moto = Motocicleta("preta", "sad332", 2)
+print(moto)
+moto.ligar_motor()
+
+carro = Carro("Branco", "dsa3232", 4)
+caminhao = Caminhao("Vermelho", "asd323", 8, False)
+
+print(moto)
+print(carro)
+print(caminhao)
---
+++ modules/Mod3/HERANCA/poo_heranca.py
@@ -0,0 +1,16 @@
+#Herança simples
+class A:
+    pass
+
+class B(A):
+    pass
+
+#Herança Multipla
+class A:
+    pass
+
+class B:
+    pass
+
+class C(A, B):
+    pass
