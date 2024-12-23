4bdc305def81e3ba65d285c441140940e8007171
Guilherme Ribeiro Souza <guilhermerib2013@hotmail.com>
2024-12-23 20:42:36 -0300

    doc_estudo.py sendo atualizado.

--- python/doc_estudo.py
+++ python/doc_estudo.py
@@ -1,16 +1,58 @@
+from abc import ABC, abstractmethod
+class Forma(ABC): #classe abstrata
+    @abstractmethod
+    def calcular_area():
+        pass    
+    @abstractmethod
+    def calcular_perimetro():
+        pass
 
-class Pato:
-    def nadar(self):
-        return "pato esta nadando"
-    
-class Peixe:
-    def nadar(self):
-        return "peixe esta nadando"
-    
-def fazer_nadar(animal): #aceita qualquer objeto com o metodo nadar.
-    print(animal.nadar())
+class Retangulo(Forma):
+    def __init__(self, largura, altura):
+        self.largura = largura
+        self.altura = altura
 
-pato = Pato()
-peixe = Peixe()
-fazer_nadar(pato)
-fazer_nadar(peixe)
+    def calcular_area(self):
+        return self.largura * self.altura
+    def calcular_perimetro(self):
+        return 2*(self.largura+self.altura)
+    
+class Circulo(Forma):
+    def __init__(self, raio, pi=3.14):
+        self.raio = raio
+        self.pi = pi
+
+    def calcular_area(self):
+        return self.pi*(2*self.raio)
+    def calcular_perimetro(self):
+        return 2*self.pi*self.raio
+formas = [Retangulo(4,5), Circulo(3)]
+for forma in formas:
+    print(f"Area: {forma.calcular_area()}, perimetro: {forma.calcular_perimetro()}")
+
+class Veiculo(ABC): #interface
+    @abstractmethod
+    def acelerar(self):
+        pass
+    
+    @abstractmethod
+    def frear(self):
+        pass
+
+class Carro(Veiculo):
+    def acelerar(self):
+        return "Carro acelerando"
+    
+    def frear(self):
+        return "Carro freando"
+    
+class Moto(Veiculo):
+    def acelerar(self):
+        return "Moto acelerando"
+    def frear(self):
+        return "Moto freando"
+    
+veiculos = [Carro(), Moto()]
+for veiculo in veiculos:
+    print(veiculo.acelerar())
+    print(veiculo.frear())
